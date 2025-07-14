0d2b37f691665c56cdf2f24d93ba6607a03f386b
Gattikowsik <kowsikgatti11@gmail.com>
2025-07-14 12:26:21 +0530

    updated test.js file

---
+++ logging-middleware/test.js
@@ -0,0 +1,11 @@
+// test.js
+import { Log } from "./log.js";
+
+// Example 1: Valid log
+Log("backend", "error", "handler", "received string, expected bool");
+
+// Example 2: Invalid stack
+Log("server", "warn", "service", "This should fail due to invalid stack");
+
+// Example 3: Valid frontend log
+Log("frontend", "info", "component", "User profile rendered successfully");
